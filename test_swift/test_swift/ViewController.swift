3cfb802c5bd65e664b088d76d21ae2b2a19cd77a
nshhhin <cs172004@meiji.ac.jp>
2017-11-20 02:39:59 +0900

    はじめてのコミット
    print("テスト")できるようにした

---
+++ test_swift/test_swift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test_swift
+//
+//  Created by 新納真次郎 on 2017/11/20.
+//  Copyright © 2017年 nshhhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("テストだよ")
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
