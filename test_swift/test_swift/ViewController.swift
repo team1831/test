fa5c77bc9d2c52f2d49c200501a641f50a2442e0
maki <tekkanomaki01@gmail.com>
2017-11-20 02:57:45 +0900

    クラスの作成を行った
    みんなでUNKOを投げつけあおうぜ

--- test_swift/test_swift/ViewController.swift
+++ test_swift/test_swift/ViewController.swift
@@ -9,18 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  var m_UNKO = [String]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     print("テストだよ")
+    
+    maki()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
+  func maki(){
+    m_UNKO[0] = "UNKO"
+    
+    print("MAKI専用の\(m_UNKO[0])クラスだぜ")
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
 
